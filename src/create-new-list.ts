2b05244fc5f4f5234dd3d59a3046095700af0cf1
Mark Carranza <mark.carranza@gmail.com>
2023-10-09 09:11:26 -0700

    Change service url

--- src/create-new-list.ts
+++ src/create-new-list.ts
@@ -1,13 +1,13 @@
 import { html } from 'lit';
 import { ModalConfig, ModalManager } from '@internetarchive/modal-manager';
 import '@internetarchive/ia-userlist-settings';
+import { userListServiceUrl } from './user-lists-service-url';
 
 export async function createNewList(): Promise<void> {
   const modalManager = document.querySelector('modal-manager') as ModalManager;
   modalManager?.setAttribute('id', 'create-user-list-modal');
 
-  const userListsServiceUrl =
-    'https://www-laton.archive.org/services/users/me/lists';
+  const createUserListsServiceUrl = `${userListServiceUrl}/me/lists`;
 
   modalManager.showModal({
     config: new ModalConfig({
@@ -20,7 +20,7 @@ export async function createNewList(): Promise<void> {
     customModalContent: html`
       <iaux-userlist-settings
         .userList=${{}}
-        .baseAPIUrl=${userListsServiceUrl}
+        .baseAPIUrl=${createUserListsServiceUrl}
         @listModalClosed=${() => {
           modalManager.closeModal();
         }}
