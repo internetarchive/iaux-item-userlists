297b9610e24cb626fb2059e5d182fcd7b1151f34
Mark Carranza <mark.carranza@gmail.com>
2023-10-16 18:21:13 -0700

    Fix close on click property setting

--- src/ia-item-user-lists.ts
+++ src/ia-item-user-lists.ts
@@ -210,7 +210,7 @@ export class IaItemUserLists extends LitElement {
           ?includeSelectedOption=${true}
           ?isCustomList=${true}
           ?closeOnEscape=${true}
-          ?closeOnOutsideClick=${true}
+          ?closeOnBackdropClick=${true}
           @click=${this.dropdownClicked}
         >
           <div class="list-title" slot="dropdown-label">${this.mainButton}</div>
