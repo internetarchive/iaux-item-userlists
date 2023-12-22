2f3a531e492bf7d53fff160fbdc4974a1c85f4d4
Mark Carranza <mark.carranza@gmail.com>
2023-12-21 18:26:13 -0800

    Move icons to modules

--- src/ia-item-user-lists.ts
+++ src/ia-item-user-lists.ts
@@ -1,23 +1,15 @@
 /**
  * Button and dropdown for adding item to user lists
  */
-import {
-  html,
-  css,
-  LitElement,
-  svg,
-  SVGTemplateResult,
-  TemplateResult,
-  nothing,
-} from 'lit';
+import { html, css, LitElement, type TemplateResult, nothing } from 'lit';
 import { property, customElement, state, query } from 'lit/decorators.js';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import type { IaDropdown } from '@internetarchive/ia-dropdown';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import {
   UserListsService,
-  UserListsServiceInterface,
-  UserList,
+  type UserListsServiceInterface,
+  type UserList,
   /* UserListMember, */
 } from '@internetarchive/ia-userlist-settings';
 import { SearchService } from '@internetarchive/search-service';
@@ -26,7 +18,11 @@ import { FetchHandler } from './fetch-handler';
 import { userListServiceUrl } from './user-lists-service-url';
 import '@internetarchive/ia-dropdown';
 import './item-user-lists';
+import spinner from './assets/images/spinner';
+import plusIcon from './assets/icons/plusIcon';
+import checkIcon from './assets/icons/checkIcon';
 
+type mainButtonStatusType = 'loading' | 'no_lists' | 'lists' | 'error';
 @customElement('ia-item-user-lists')
 export class IaItemUserLists extends LitElement {
   /**
@@ -50,6 +46,9 @@ export class IaItemUserLists extends LitElement {
       baseUrl: userListServiceUrl,
     });
 
+  @state() private mainButtonStatus: mainButtonStatusType = 'loading';
+
+  // ??? is this used?
   @query('ia-dropdown') private dropdown!: IaDropdown;
 
   constructor() {
@@ -68,7 +67,7 @@ export class IaItemUserLists extends LitElement {
     );
 
     // Listen for create List event from create-new-list
-    const createEventListener = (e: CustomEvent) => {
+    const newListEventListener = (e: CustomEvent) => {
       // TEMP: Set selected count for main button icon state
       this.selectedCount += 1;
 
@@ -87,13 +86,11 @@ export class IaItemUserLists extends LitElement {
     window.addEventListener(
       'createUserList',
       // eslint-disable-next-line no-undef
-      createEventListener as EventListener
+      newListEventListener as EventListener
     );
   }
 
-  connectedCallback(): void {
-    super.connectedCallback?.();
-    // Set userlist data
+  firstUpdated(): void {
     this.initUserLists();
   }
 
@@ -120,46 +117,36 @@ export class IaItemUserLists extends LitElement {
         item => item.item_is_member
       ).length;
 
-      // Not sure this will work
-      this.requestUpdate();
+      this.mainButtonStatus = this.selectedCount === 0 ? 'no_lists' : 'lists';
     } else {
       // eslint-disable-next-line no-console
       console.error('Error loading userlist data', result.error);
+      // eslint-disable-next-line no-alert
+      window.alert('Error loading userlist data');
+      this.mainButtonStatus = 'error';
     }
   }
 
-  get checkIcon(): SVGTemplateResult {
-    return svg`<svg viewBox="0 0 100 100"
-    xmlns="http://www.w3.org/2000/svg"
-    style="width: 16px; height: 16px;"
-    >
-    <path
-      d="m33.3333333 90-33.3333333-33.3333333 13.3333333-13.3333334 20 20 53.3333334-53.3333333 13.3333333 13.3333333z"
-      fill-rule="evenodd"
-    />
-  </svg>`;
-  }
-
-  get plusIcon(): SVGTemplateResult {
-    return svg`<svg
-      viewBox="0 0 100 100"
-      xmlns="http://www.w3.org/2000/svg"
-      style="width: 16px; height: 16px;"
-      >
-    <g fill-rule="nonzero">
-      <path d="m49.9459358 0c13.7978412 0 25.5896453 4.88776371 35.3754122 14.6632911 9.7857669 9.7755275 14.678652 21.554993 14.678652 35.3383967 0 13.7800281-4.8928851 25.5594936-14.678652 35.3350211-9.7857669 9.7755274-21.577571 14.6632911-35.3754122 14.6632911s-25.5716235-4.8877637-35.3213471-14.6632911c-9.74972353-9.7755275-14.6245887-21.554993-14.6245887-35.3383967-.00224931-9.0014064 2.23243779-17.3524613 6.70406469-25.0531645 4.47162691-7.7007033 10.54380341-13.7834037 18.21652941-18.24810129 7.6727261-4.46469761 16.0145067-6.69704641 25.0253417-6.69704641zm0 6c-7.9548389 0-15.2549008 1.95357387-22.0076943 5.8829701-6.7720278 3.9405885-12.0963254 9.2741139-16.0455165 16.0751171-3.93842488 6.7824623-5.89471047 14.0931257-5.89272481 22.040225 0 12.1941053 4.24437231 22.4500702 12.87283241 31.1013666 8.6242501 8.6470752 18.8695883 12.9003212 31.0731032 12.9003212 12.2065273 0 22.4734846-4.2557068 31.1349929-12.9081521 8.6603017-8.6512398 12.9190715-18.9040965 12.9190715-31.0935357l-.0036695-.6147591c-.1419586-11.9183989-4.4018851-21.9707923-12.915402-30.475401-8.6615083-8.6524453-18.9284656-12.9081521-31.1349929-12.9081521z" />
-      <path d="m56 23v22h22v11h-22v22h-11l-.001-22h-21.999v-11h21.999l.001-22z" />
-    </g>
-  </svg>`;
+  get mainButtonIcon(): TemplateResult {
+    switch (this.mainButtonStatus) {
+      case 'loading':
+        return spinner;
+      case 'no_lists':
+        return plusIcon;
+      case 'lists':
+        return checkIcon;
+      case 'error':
+        return plusIcon;
+      default:
+        return spinner;
+    }
   }
 
   get mainButton(): TemplateResult {
     return html`
       <div class="action-bar-text">
         <ia-icon-label @click=${this.mainButtonClicked}>
-          <div slot="icon" class="icon-img">
-            ${this.selectedCount > 0 ? this.checkIcon : this.plusIcon}
-          </div>
+          <div slot="icon" class="icon-img">${this.mainButtonIcon}</div>
           <div class="label">Add to list</div>
           <div class="label-sm">Lists</div>
         </ia-icon-label>
@@ -224,6 +211,8 @@ export class IaItemUserLists extends LitElement {
       <div class="list-container">
         <ia-dropdown
           class="list-dropdown"
+          disabled=${this.mainButtonStatus === 'loading' ||
+          this.mainButtonStatus === 'error'}
           ?openViaCaret=${false}
           ?closeOnSelect=${true}
           ?includeSelectedOption=${true}
@@ -247,11 +236,14 @@ export class IaItemUserLists extends LitElement {
 
     ia-icon-label {
       cursor: pointer;
-    }
-
-    ia-icon-label {
       display: flex;
     }
+
+    .icon-img {
+      height: 16px;
+      width: 16px;
+    }
+
     div.list-title {
       font-weight: 600;
       color: #2c2c2c;
@@ -311,11 +303,6 @@ export class IaItemUserLists extends LitElement {
       height: 100vh;
       z-index: 1;
       background-color: transparent;
-    }
-
-    svg {
-      height: 30px;
-      width: 30px;
     }
   `;
 }
