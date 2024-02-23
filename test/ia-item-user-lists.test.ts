2bd95ab77c38ef8fb8e0351cb23790cb92b4e61f
Mark Carranza <mark.carranza@gmail.com>
2024-02-22 20:13:05 -0800

    Mock service unit tests

--- test/ia-item-user-lists.test.ts
+++ test/ia-item-user-lists.test.ts
@@ -9,9 +9,10 @@ import '../src/ia-item-user-lists';
 
 describe('IAItemUserlists', () => {
   let mockCreateUserListsService: sinon.SinonStub;
-  const mockUserListsService = sinon.createStubInstance(UserListsService);
+  let mockUserListsService: sinon.SinonStubbedInstance<UserListsService>;
 
   beforeEach(() => {
+    mockUserListsService = sinon.createStubInstance(UserListsService);
     mockCreateUserListsService = sinon.stub().returns(mockUserListsService);
 
     sinon
@@ -24,6 +25,8 @@ describe('IAItemUserlists', () => {
   });
 
   it('creates UserListsService and loads item', async () => {
+    mockUserListsService.fetchOwnListsContainingItem.resolves({ success: [] });
+
     const el = await fixture<IaItemUserLists>(
       html`<ia-item-user-lists item="goody"></ia-item-user-lists>`
     );
@@ -38,5 +41,40 @@ describe('IAItemUserlists', () => {
       mockUserListsService.fetchOwnListsContainingItem,
       'goody'
     );
+
+    const button = el.shadowRoot
+      ?.querySelector('ia-dropdown')
+      ?.shadowRoot?.querySelector('.click-main') as HTMLButtonElement;
+    expect(button).to.not.have.attribute('disabled');
+
+    const label = el.shadowRoot?.querySelector('div.label')?.innerHTML;
+    expect(label).to.contain('Add to list');
+  });
+
+  it('is disabled on error', async () => {
+    mockUserListsService.fetchOwnListsContainingItem.throws();
+
+    const el = await fixture<IaItemUserLists>(
+      html`<ia-item-user-lists item="goody"></ia-item-user-lists>`
+    );
+
+    await expect(el).shadowDom.to.be.accessible();
+
+    sinon.assert.calledOnce(mockCreateUserListsService);
+
+    sinon.assert.calledOnce(mockUserListsService.fetchOwnListsContainingItem);
+
+    sinon.assert.calledWith(
+      mockUserListsService.fetchOwnListsContainingItem,
+      'goody'
+    );
+
+    const button = el.shadowRoot
+      ?.querySelector('ia-dropdown')
+      ?.shadowRoot?.querySelector('.click-main') as HTMLButtonElement;
+    expect(button).to.have.attribute('disabled');
+
+    const label = el.shadowRoot?.querySelector('div.label')?.innerHTML;
+    expect(label).to.contain('Load Error');
   });
 });
